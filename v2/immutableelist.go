75c8d456d9121656ef5a7ee78d1da3b7e14c2575
JDY <jocelyn@barry-dumay.com>
2022-01-05 09:55:59 +0100

    [ecore] add immutableEList class

---
+++ v2/immutableelist.go
@@ -0,0 +1,163 @@
+// *****************************************************************************
+// Copyright(c) 2021 MASA Group
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// *****************************************************************************
+
+package ecore
+
+import "strconv"
+
+type emptyImmutableEList[T any] struct {
+}
+
+func (l *emptyImmutableEList[T]) Add(t T) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) AddAll(c Collection[T]) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) Insert(index int, t T) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) InsertAll(index int, c Collection[T]) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) MoveObject(newIndex int, t T) {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) MoveIndex(oldIndex, newIndex int) T {
+	panic("Immutable list can't be modified")
+}
+
+// Get an element of the array
+func (l *emptyImmutableEList[T]) Get(index int) T {
+	panic("Index out of bounds: index=" + strconv.Itoa(index) + " size=" + strconv.Itoa(l.Size()))
+}
+
+func (l *emptyImmutableEList[T]) Set(index int, t T) T {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) RemoveAt(index int) T {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) Remove(t T) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) RemoveAll(c Collection[T]) bool {
+	panic("Immutable list can't be modified")
+}
+
+func (l *emptyImmutableEList[T]) RetainAll(c Collection[T]) bool {
+	panic("Immutable list can't be modified")
+}
+
+// Size count the number of element in the array
+func (l *emptyImmutableEList[T]) Size() int {
+	return 0
+}
+
+func (l *emptyImmutableEList[T]) Clear() {
+	panic("Immutable list can't be modified")
+}
+
+// Empty return true if the array contains 0 element
+func (l *emptyImmutableEList[T]) Empty() bool {
+	return true
+}
+
+// Contains return if an array contains or not an element
+func (l *emptyImmutableEList[T]) Contains(t T) bool {
+	return false
+}
+
+// IndexOf return the index on an element in an array, else return -1
+func (l *emptyImmutableEList[T]) IndexOf(t T) int {
+	return -1
+}
+
+// Iterator through the array
+func (l *emptyImmutableEList[T]) Iterator() Iterator[T] {
+	return &eListIterator[T]{list: l}
+}
+
+// ToArray convert to array
+func (l *emptyImmutableEList[T]) ToArray() []T {
+	return []T{}
+}
+
+func (l *emptyImmutableEList[T]) GetUnResolvedList() EList[T] {
+	return l
+}
+
+func NewEmptyImmutableEList[T any]() *emptyImmutableEList[T] {
+	return &emptyImmutableEList[T]{}
+}
+
+type immutableEList[T comparable] struct {
+	emptyImmutableEList[T]
+	data []T
+}
+
+// NewImmutableEList return a new ImmutableEList
+func NewImmutableEList[T comparable](data []T) *immutableEList[T] {
+	return &immutableEList[T]{data: data}
+}
+
+// Get an element of the array
+func (l *immutableEList[T]) Get(index int) T {
+	if index < 0 || index >= l.Size() {
+		panic("Index out of bounds: index=" + strconv.Itoa(index) + " size=" + strconv.Itoa(l.Size()))
+	}
+	return l.data[index]
+}
+
+// Size count the number of element in the array
+func (l *immutableEList[T]) Size() int {
+	return len(l.data)
+}
+
+// Empty return true if the array contains 0 element
+func (l *immutableEList[T]) Empty() bool {
+	return l.Size() == 0
+}
+
+// Contains return if an array contains or not an element
+func (l *immutableEList[T]) Contains(t T) bool {
+	return l.IndexOf(t) != -1
+}
+
+// IndexOf return the index on an element in an array, else return -1
+func (l *immutableEList[T]) IndexOf(t T) int {
+	for i, value := range l.data {
+		if value == t {
+			return i
+		}
+	}
+	return -1
+}
+
+// Iterator through the array
+func (l *immutableEList[T]) Iterator() Iterator[T] {
+	return &eListIterator[T]{list: l}
+}
+
+// ToArray convert to array
+func (l *immutableEList[T]) ToArray() []T {
+	return l.data
+}
+
+func (l *immutableEList[T]) GetUnResolvedList() EList[T] {
+	return l
+}
---
+++ v2/immutableelist_test.go
@@ -0,0 +1,207 @@
+// *****************************************************************************
+// Copyright(c) 2021 MASA Group
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// *****************************************************************************
+
+package ecore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyImmutableAdd(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Add(1) })
+}
+
+func TestEmptyImmutableAddAll(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	b := NewImmutableEList[int]([]int{1, 2})
+	assert.Panics(t, func() { a.AddAll(b) })
+}
+
+func TestEmptyImmutableInsert(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Insert(0, 1) })
+}
+
+func TestEmptyImmutableInsertAll(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	b := NewImmutableEList[int]([]int{1, 2})
+	assert.Panics(t, func() { a.InsertAll(0, b) })
+}
+
+func TestEmptyImmutableMoveObject(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.MoveObject(0, 1) })
+}
+
+func TestEmptyImmutableMove(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.MoveIndex(0, 1) })
+}
+
+func TestEmptyImmutableGet(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Get(0) })
+}
+
+func TestEmptyImmutableSet(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Set(0, 1) })
+}
+
+func TestEmptyImmutableRemoveAt(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.RemoveAt(0) })
+}
+
+func TestEmptyImmutableRemove(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Remove(0) })
+}
+
+func TestEmptyImmutableSize(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Equal(t, 0, a.Size())
+}
+
+func TestEmptyImmutableEmpty(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.True(t, a.Empty())
+}
+
+func TestEmptyImmutableClear(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.Clear() })
+}
+
+func TestEmptyImmutableRemoveAll(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Panics(t, func() { a.RemoveAll(nil) })
+}
+
+func TestEmptyImmutableContains(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.False(t, a.Contains(3))
+}
+
+func TestEmptyImmutableIndexOf(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Equal(t, -1, a.IndexOf(3))
+}
+
+func TestEmptyImmutableToArray(t *testing.T) {
+	a := &emptyImmutableEList[int]{}
+	assert.Equal(t, []int{}, a.ToArray())
+}
+
+func TestEmptyImmutableGetUnResolved(t *testing.T) {
+	l := &emptyImmutableEList[int]{}
+	assert.Equal(t, l, l.GetUnResolvedList())
+}
+
+func TestImmutableEListAdd(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.Add(1) })
+}
+
+func TestImmutableEListAddAll(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	b := NewImmutableEList[int]([]int{1, 2})
+	assert.Panics(t, func() { a.AddAll(b) })
+}
+
+func TestImmutableEListInsert(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.Insert(0, 1) })
+}
+
+func TestImmutableEListInsertAll(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	b := NewImmutableEList[int]([]int{1, 2})
+	assert.Panics(t, func() { a.InsertAll(0, b) })
+}
+
+func TestImmutableEListMoveObject(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.MoveObject(0, 1) })
+}
+
+func TestImmutableEListMove(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.MoveIndex(0, 1) })
+}
+
+func TestImmutableEListGet(t *testing.T) {
+	a := NewImmutableEList[int]([]int{1, 2})
+	assert.Equal(t, 1, a.Get(0))
+	assert.Equal(t, 2, a.Get(1))
+	assert.Panics(t, func() { a.Get(2) })
+}
+
+func TestImmutableEListSet(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.Set(0, 1) })
+}
+
+func TestImmutableEListRemoveAt(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.RemoveAt(0) })
+}
+
+func TestImmutableEListRemove(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.Remove(0) })
+}
+
+func TestImmutableEListSize(t *testing.T) {
+	a := NewImmutableEList[int]([]int{1, 2})
+	assert.Equal(t, 2, a.Size())
+}
+
+func TestImmutableEListEmpty(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.True(t, a.Empty())
+}
+
+func TestImmutableEListClear(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.Clear() })
+}
+
+func TestImmutableEListRemoveAll(t *testing.T) {
+	a := NewImmutableEList[int](nil)
+	assert.Panics(t, func() { a.RemoveAll(nil) })
+}
+
+func TestImmutableEListContains(t *testing.T) {
+	a := NewImmutableEList[int]([]int{1, 2})
+	assert.True(t, a.Contains(2))
+	assert.False(t, a.Contains(3))
+	b := NewImmutableEList[int](nil)
+	assert.False(t, b.Contains(3))
+}
+
+func TestImmutableEListIterate(t *testing.T) {
+	var iDatas []int
+	iDatas = append(iDatas, 0, 2, 4, 6)
+	arr := NewImmutableEList[int](iDatas)
+	i := 0
+	for it := arr.Iterator(); it.HasNext(); {
+		assert.Equal(t, it.Next(), i)
+		i += 2
+	}
+	assert.Equal(t, 8, i)
+}
+
+func TestImmutableEListGetUnResolved(t *testing.T) {
+	l := NewImmutableEList[int]([]int{})
+	assert.Equal(t, l, l.GetUnResolvedList())
+}
