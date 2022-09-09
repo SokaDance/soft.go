60baf9c55c79ada67632f13e0612c611c5beff1f
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-09-09 17:51:33 +0200

    [stream] add Parallel/Sequential/IsParallel methods in Stream interface and test them

--- stream/stream.go
+++ stream/stream.go
@@ -1,6 +1,21 @@
 package stream
 
 type Stream interface {
+	// Parallel returns an equivalent stream that is parallel.  May return
+	// itself, either because the stream was already parallel, or because
+	// the underlying stream state was modified to be parallel.
+	Parallel() Stream
+
+	// Sequential returns an equivalent stream that is sequential.  May return
+	// itself, either because the stream was already sequential, or because
+	// the underlying stream state was modified to be sequential.
+	Sequential() Stream
+
+	// IsParallel returns whether this stream, if a terminal operation were to be executed,
+	// would execute in parallel.  Calling this method after invoking an
+	// terminal stream operation method may yield unpredictable results.
+	IsParallel() bool
+
 	// stateless operation
 	Filter(predicate func(any) bool) Stream
 	// FlatMap(mapper func(any) Stream) Stream
--- stream/stream_test.go
+++ stream/stream_test.go
@@ -4,19 +4,36 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestAnyMatch(t *testing.T) {
+func TestStream_IsParallel(t *testing.T) {
+	s := OfSlice([]any{})
+	assert.False(t, s.IsParallel())
+	s = s.Parallel()
+	require.NotNil(t, s)
+	assert.True(t, s.IsParallel())
+	s = s.Sequential()
+	require.NotNil(t, s)
+	assert.False(t, s.IsParallel())
+}
+
+func TestStream_AnyMatch_Sequential(t *testing.T) {
 	assert.True(t, OfSlice([]any{1, 2, 3}).AnyMatch(func(a any) bool { return a == 2 }))
 	assert.False(t, OfSlice([]any{1, 2, 3}).AnyMatch(func(a any) bool { return a == 0 }))
 }
 
-func TestAllMatch(t *testing.T) {
+// func TestStream_AnyMatch_Parallel(t *testing.T) {
+// 	assert.True(t, OfSlice([]any{1, 2, 3}).Parallel().AnyMatch(func(a any) bool { return a == 2 }))
+// 	assert.False(t, OfSlice([]any{1, 2, 3}).Parallel().AnyMatch(func(a any) bool { return a == 0 }))
+// }
+
+func TestStream_AllMatch_Sequential(t *testing.T) {
 	assert.True(t, OfSlice([]any{2, 2, 2}).AllMatch(func(a any) bool { return a == 2 }))
 	assert.False(t, OfSlice([]any{2, 2, 2}).AllMatch(func(a any) bool { return a == 3 }))
 }
 
-func TestNoneMatch(t *testing.T) {
+func TestStream_NoneMatch_Sequential(t *testing.T) {
 	assert.True(t, OfSlice([]any{2, 2, 2}).NoneMatch(func(a any) bool { return a == 3 }))
 	assert.False(t, OfSlice([]any{2, 2, 2}).NoneMatch(func(a any) bool { return a == 2 }))
 }
