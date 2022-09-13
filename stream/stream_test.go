e682acedf01763dc969f0da99e5349ffb05dbab8
JDY <jocelyn.dumay@masagroup.net>
2022-09-13 17:36:00 +0200

    [stream] add Min/Max/Reduce/ReduceWith tests

--- stream/stream_test.go
+++ stream/stream_test.go
@@ -39,11 +39,6 @@ func TestStream_AnyMatch_Sequential(t *testing.T) {
 	assert.False(t, OfSlice([]any{1, 2, 3}).AnyMatch(func(a any) bool { return a == 0 }))
 }
 
-// func TestStream_AnyMatch_Parallel(t *testing.T) {
-// 	assert.True(t, OfSlice([]any{1, 2, 3}).Parallel().AnyMatch(func(a any) bool { return a == 2 }))
-// 	assert.False(t, OfSlice([]any{1, 2, 3}).Parallel().AnyMatch(func(a any) bool { return a == 0 }))
-// }
-
 func TestStream_AllMatch_Sequential(t *testing.T) {
 	assert.True(t, OfSlice([]any{2, 2, 2}).AllMatch(func(a any) bool { return a == 2 }))
 	assert.False(t, OfSlice([]any{2, 2, 2}).AllMatch(func(a any) bool { return a == 3 }))
@@ -52,4 +47,20 @@ func TestStream_AllMatch_Sequential(t *testing.T) {
 func TestStream_NoneMatch_Sequential(t *testing.T) {
 	assert.True(t, OfSlice([]any{2, 2, 2}).NoneMatch(func(a any) bool { return a == 3 }))
 	assert.False(t, OfSlice([]any{2, 2, 2}).NoneMatch(func(a any) bool { return a == 2 }))
+}
+
+func TestStream_Min_Sequential(t *testing.T) {
+	assert.Equal(t, 1, OfSlice([]any{3, 2, 1}).Min(func(a1, a2 any) int { return a1.(int) - a2.(int) }).ElseZero())
+}
+
+func TestStream_Max_Sequential(t *testing.T) {
+	assert.Equal(t, 3, OfSlice([]any{3, 2, 1}).Max(func(a1, a2 any) int { return a1.(int) - a2.(int) }).ElseZero())
+}
+
+func TestStream_Reduce(t *testing.T) {
+	assert.Equal(t, 6, OfSlice([]any{1, 2, 3}).Reduce(func(a1, a2 any) any { return a1.(int) + a2.(int) }).ElseZero())
+}
+
+func TestStream_ReduceWith(t *testing.T) {
+	assert.Equal(t, 7, OfSlice([]any{1, 2, 3}).ReduceWith(1, func(a1, a2 any) any { return a1.(int) + a2.(int) }))
 }
