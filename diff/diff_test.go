9c8bd2dd56ca8d09975a9062a1e6394fbcbefd95
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-12-12 11:40:34 +0100

    [diff] add diff stress test

--- diff/diff_test.go
+++ diff/diff_test.go
@@ -1,10 +1,12 @@
 package diff
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"github.com/zyedidia/generic"
 	"github.com/zyedidia/generic/set"
 )
@@ -102,4 +104,141 @@ func TestCompute_Update(t *testing.T) {
 	assert.Equal(t, 0, result.GetMoves().Size())
 	assert.Equal(t, 0, result.GetDeletes().Size())
 	assert.True(t, result.GetUpdates().Equal(set.NewMapset(0)))
+}
+
+const nbIteration = 1
+const collectionSize = 5
+
+func performRandomActions(original []*mock.Mock, minimumCountAfterActions, maximumCountAfterActions int, objectToID map[*mock.Mock]uint64) []*mock.Mock {
+	destination := make([]*mock.Mock, len(original))
+	copy(destination, original)
+	destination = performInsertActions(destination, rand.Intn(maximumCountAfterActions)/2, objectToID)
+	destination = performDeleteActions(destination, rand.Intn(maximumCountAfterActions)/2)
+	destination = performMoveActions(destination, rand.Intn(maximumCountAfterActions)/3)
+	destination = performUpdateActions(destination, rand.Intn(maximumCountAfterActions)/3, objectToID)
+	if len(destination) < minimumCountAfterActions {
+		destination = performInsertActions(destination, minimumCountAfterActions, objectToID)
+	}
+	if len(destination) > maximumCountAfterActions {
+		destination = performInsertActions(destination, minimumCountAfterActions, objectToID)
+	}
+	if len(destination) > maximumCountAfterActions {
+		destination = performDeleteActions(destination, len(destination)-maximumCountAfterActions)
+	}
+	return destination
+}
+
+func scliceInsert[T any](array []T, i int, v T) []T {
+	var empty T
+	array = append(array, empty)
+	copy(array[i+1:], array[i:])
+	array[i] = v
+	return array
+}
+
+func scliceDelete[T any](array []T, i int) []T {
+	var empty T
+	copy(array[i:], array[i+1:])
+	array[len(array)-1] = empty
+	array = array[:len(array)-1]
+	return array
+}
+
+func scliceMove[T any](array []T, from, to int) []T {
+	if from != to {
+		object := array[from]
+		if to < from {
+			copy(array[to+1:], array[to:from])
+		} else {
+			copy(array[from:], array[from+1:to+1])
+		}
+		array[to] = object
+	}
+	return array
+}
+
+func performInsertActions(array []*mock.Mock, count int, objectToID map[*mock.Mock]uint64) []*mock.Mock {
+	for i := 0; i < count; i++ {
+		// create object
+		mockObject := &mock.Mock{}
+		objectToID[mockObject] = uint64(len(array) + i)
+		// compute random index
+		index := rand.Intn(len(array))
+		// apply to array
+		array = scliceInsert(array, index, mockObject)
+	}
+	return array
+}
+
+func performDeleteActions(array []*mock.Mock, count int) []*mock.Mock {
+	for i := 0; i < count; i++ {
+		// compute random index
+		index := rand.Intn(len(array))
+		// apply to array
+		array = scliceDelete(array, index)
+	}
+	return array
+}
+
+func performUpdateActions(array []*mock.Mock, count int, objectToID map[*mock.Mock]uint64) []*mock.Mock {
+	for i := 0; i < count; i++ {
+		// create new object
+		mockObject := &mock.Mock{}
+		// compute random index
+		index := rand.Intn(len(array))
+		// apply to array
+		oldObject := array[index]
+		array[index] = mockObject
+		objectToID[mockObject] = objectToID[oldObject]
+	}
+	return array
+}
+
+func performMoveActions(array []*mock.Mock, count int) []*mock.Mock {
+	for i := 0; i < count; i++ {
+		// compute random index
+		from := rand.Intn(len(array))
+		to := rand.Intn(len(array))
+		// apply to array
+		array = scliceMove(array, from, to)
+	}
+	return array
+}
+
+func applyResult(currentObjects []*mock.Mock, expectedObjects []*mock.Mock, result Result) []*mock.Mock {
+	resultObjects := make([]*mock.Mock, len(currentObjects))
+	copy(resultObjects, currentObjects)
+	result.GetDeletes().Each(func(i int) {
+		resultObjects = scliceDelete(resultObjects, i)
+	})
+	result.GetInserts().Each(func(i int) {
+		resultObjects = scliceInsert(resultObjects, i, expectedObjects[i])
+	})
+	result.GetMoves().Each(func(mi MoveIndex) {
+		resultObjects = scliceMove(resultObjects, mi.From, mi.To)
+	})
+	return resultObjects
+}
+
+func TestCompute_Stress(t *testing.T) {
+
+	objectToID := map[*mock.Mock]uint64{}
+
+	// build objects
+	currentObjects := make([]*mock.Mock, collectionSize)
+	for i := 0; i < collectionSize; i++ {
+		mockObject := &mock.Mock{}
+		objectToID[mockObject] = uint64(i)
+		currentObjects[i] = mockObject
+	}
+
+	// perform random actions & compute diff
+	for i := 0; i < nbIteration; i++ {
+		expectedObjects := performRandomActions(currentObjects, collectionSize/10, collectionSize*10, objectToID)
+		result := Compute(currentObjects, expectedObjects, generic.Equals[*mock.Mock], func(m *mock.Mock) uint64 { return objectToID[m] })
+		resultObjects := applyResult(currentObjects, expectedObjects, result)
+		require.Equal(t, expectedObjects, resultObjects)
+		currentObjects = resultObjects
+	}
+
 }
