8d9bde76f06f8670c0499de2dacb5d8e09b136f0
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-12-13 11:30:38 +0100

    [diff] diff accept visitor implementation

--- diff/diff.go
+++ diff/diff.go
@@ -8,7 +8,10 @@ type Entry[T any] struct {
 	IsAddition bool
 }
 
-type Diff[T any] []Entry[T]
+type Diff[T any] struct {
+	Entries []Entry[T]
+	equals  EqualsFn[T]
+}
 
 type DiffVisitor[T any] interface {
 	HandleAdd(index int, element T)
@@ -17,8 +20,79 @@ type DiffVisitor[T any] interface {
 	HandleReplace(index int, oldElement T, newElement T)
 }
 
-func (d Diff[T]) Accept(v DiffVisitor[T]) {
+func (d Diff[T]) Accept(visitor DiffVisitor[T]) {
+	for i := 0; i < len(d.Entries); i++ {
+		entry := d.Entries[i]
+		elem := entry.Element
+		pos := entry.Index
+		add := entry.IsAddition
+		if i+1 < len(d.Entries) {
+			nextEntry := d.Entries[i+1]
+			if add != nextEntry.IsAddition {
+				var addPos, removePos int
+				var addElem, removeElem T
+				if add {
+					addPos = pos
+					addElem = elem
+					removePos = nextEntry.Index
+					removeElem = nextEntry.Element
+					if addPos > removePos {
+						// a b c d e f -- start
+						// a b c b d e f -- add b at 4
+						// a c b d e f -- remove b at 2
 
+						// net effect is the same as:
+						// a b c d e f -- start
+						// a c d e f -- remove b at 2
+						// a c b d e f -- add b at 3
+						addPos--
+					} else if removePos > addPos {
+						// a b c d e f -- start
+						// a d b c d e f -- add d at 2
+						// a d b c e f -- remove d at 5
+
+						// net effect is the same as
+						// a b c d e f -- start
+						// a b c e f -- remove d at 4
+						// a d b c d e f -- add d at 2
+
+						// So we adjust the remove index to fit the indices
+						// of the remove-then-add scenario
+						removePos--
+					} else {
+						// rare case: element is added and then immediately
+						// removed. Handle the add entry and then continue
+						// iterating starting at the remove entry
+						visitor.HandleAdd(pos, elem)
+						continue
+					}
+				} else {
+					removePos = pos
+					removeElem = elem
+
+					addPos = nextEntry.Index
+					addElem = nextEntry.Element
+				}
+				if removePos == addPos {
+					visitor.HandleReplace(pos, removeElem, addElem)
+					i++
+					continue
+				}
+
+				if d.equals(removeElem, addElem) {
+					visitor.HandleMove(removePos, addPos, elem)
+					i++
+					continue
+				}
+
+			}
+		}
+		if add {
+			visitor.HandleAdd(pos, elem)
+		} else {
+			visitor.HandleRemove(pos, elem)
+		}
+	}
 }
 
 func insert[T any](arr []T, index int, t T) []T {
@@ -45,12 +119,12 @@ func indexOf[T any](array []T, v T, index int, equals EqualsFn[T]) int {
 	return -1
 }
 
-func Compute[T any](immutableOldArray []T, newArray []T, equals EqualsFn[T]) Diff[T] {
+func Compute[T any](immutableOldArray []T, immutableNewArray []T, equals EqualsFn[T]) Diff[T] {
 	index := 0
 	entries := []Entry[T]{}
 	oldArray := make([]T, len(immutableOldArray))
 	copy(oldArray, immutableOldArray)
-	for _, newValue := range newArray {
+	for _, newValue := range immutableNewArray {
 		if len(oldArray) <= index {
 			// append newValue to newArray
 			entries = append(entries, Entry[T]{Index: index, Element: newValue, IsAddition: true})
@@ -61,7 +135,7 @@ func Compute[T any](immutableOldArray []T, newArray []T, equals EqualsFn[T]) Dif
 				oldValue := oldArray[index]
 				if !equals(oldValue, newValue) {
 					if oldIndexOfNewValue := indexOf(oldArray, newValue, index, equals); oldIndexOfNewValue != -1 {
-						if newIndexOfOldValue := indexOf(newArray, oldValue, index, equals); newIndexOfOldValue == -1 {
+						if newIndexOfOldValue := indexOf(immutableNewArray, oldValue, index, equals); newIndexOfOldValue == -1 {
 							// removing oldValue from list[index]
 							entries = append(entries, Entry[T]{Index: index, Element: oldValue, IsAddition: false})
 							oldArray = remove(oldArray, index)
@@ -107,5 +181,5 @@ func Compute[T any](immutableOldArray []T, newArray []T, equals EqualsFn[T]) Dif
 		// remove excess trailing elements not present in newArray
 		entries = append(entries, Entry[T]{Index: i, Element: oldArray[i], IsAddition: false})
 	}
-	return Diff[T](entries)
+	return Diff[T]{Entries: entries, equals: equals}
 }
